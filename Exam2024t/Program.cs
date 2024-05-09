ab12d5b49d8946966fefc3285125d6b439baa4b0
Magnus Møller Jensen <eaa23mmje@students.eaaa.dk>
2024-05-09 12:52:24 +0200

    crazy

--- Exam2024t/Program.cs
+++ Exam2024t/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Exam2024t.Services;
 
 namespace Exam2024t
 {
@@ -12,6 +13,10 @@ namespace Exam2024t
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton<IApplicationService, ApplicationServiceInMemory>();
+
+            builder.Services.AddSingleton<IAdminService, AdminServiceInMemory>();
 
             await builder.Build().RunAsync();
         }

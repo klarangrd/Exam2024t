dafd4ded4a9048c6bf0ad0c552a28c1df4987d93
Magnus Møller Jensen <eaa23mmje@students.eaaa.dk>
2024-05-16 10:56:52 +0200

    filter

--- Exam2024t/Services/ApplicationServiceInMemory.cs
+++ Exam2024t/Services/ApplicationServiceInMemory.cs
@@ -9,7 +9,7 @@ namespace Exam2024t.Services
         private static List<Application> Applications = new List<Application>(){ new Application
     {
         IsVolunteer = false,
-        FirstpriorityWeek = "uge 28",
+        FirstpriorityWeek = "Uge 28",
         FirstpriorityPeriod ="Onsdag - Søndag",
         SecondpriorityWeek = "Uge 30",
         SecondpriorityPeriod ="Lørdag - Søndag",

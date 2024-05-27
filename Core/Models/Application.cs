b706465ebffb180e98b4264db8016ab4505a37c5
Magnus Møller Jensen <eaa23mmje@students.eaaa.dk>
2024-05-27 11:47:08 +0200

    helllo world

--- Core/Models/Application.cs
+++ Core/Models/Application.cs
@@ -41,6 +41,8 @@ namespace Core.Models
 
         public bool Issignedupfornewsletter { get; set; }
 
+        public bool IsParent { get; set; }
+
         public Child Child { get; set; }
 
         public  Signature Signature { get; set; }

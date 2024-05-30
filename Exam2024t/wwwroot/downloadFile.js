d706b80c85f2be02ca2d4e8875d883ad8c460197
klarangrd <klara.ngaard@gmail.com>
2024-05-30 10:50:59 +0200

    pdf test

---
+++ Exam2024t/wwwroot/downloadFile.js
@@ -0,0 +1,15 @@
+﻿window.downloadFile = function (url) {
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'application.pdf';  //file name
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(() => alert('Failed to download file.'));
+};
